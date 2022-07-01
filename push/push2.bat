0f2f6dffc3531df530c3f57814cba002eee333b0
l8827974 <lijinyang8827974@163.com>
2022-07-01 08:21:14 +0800

    bat鎵瑰鐞嗚嚜鍔ㄦ帹閫?2022/07/01, 8:21:14

--- push/push2.bat
+++ push/push2.bat
@@ -4,8 +4,8 @@
 cd D:\Desktop\play\github
 :: 自动提交
 git init 
-git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+git add *
+git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
 @echo 已经完成,
