52a74526fa4d1dd2ae5ca8df77e0370ba4bc37fc
l8827974 <lijinyang8827974@163.com>
2022-07-01 08:27:17 +0800

    2022/07/01 周五  8:27:17.73

--- push/push.bat
+++ push/push.bat
@@ -1,11 +1,14 @@
-echo "1. Move to working directory"\n
+echo "1. Move to working directory"
+
 
 cd D:\Desktop\play\github
 
-echo "2. Start submitting code to the local repository"\n
+echo "2. Start submitting code to the local repository"
+
 git add -A
  
-echo "3. Commit the changes to the local repository"\n
+echo "3. Commit the changes to the local repository"
+
 set now=%date% %time%
 echo "Time:" %now%\n
 git commit -m "%now%"
